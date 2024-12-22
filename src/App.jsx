ed14131646972acde798d961fdd40eba5ebe32ae
PhillipSegura <PhillipaSegura@gmail.com>
2024-12-22 13:27:06 -0700

    Installed axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import Test from "./components/Test";
-import NewFile from "./components/NewFile";
-import Name from "./components/Name";
 
 function App() {
-  return (
-    <>
-      <NewFile />
-      <h1> How to Kill off DEvon's character </h1>
-      <Test />
-      <Name />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
