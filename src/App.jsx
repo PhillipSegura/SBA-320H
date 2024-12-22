3a5422d4f2e5042deef72357e88c8329c46a0914
PhillipSegura <PhillipaSegura@gmail.com>
2024-12-22 13:20:41 -0700

    down loaded and set up all my folders and tested my REACT

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Test from "./components/Test";
+import NewFile from "./components/NewFile";
+import Name from "./components/Name";
+
+function App() {
+  return (
+    <>
+      <NewFile />
+      <h1> How to Kill off DEvon's character </h1>
+      <Test />
+      <Name />
+    </>
+  );
+}
+
+export default App;
